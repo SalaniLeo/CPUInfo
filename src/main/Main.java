f069058099ccec8434e05b8b7be8e977cc400aee
SalaniLeo <leonardo07.salani@gmail.com>
2022-09-19 21:22:12 +0200

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,9 @@
 package main;
 
-import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
+import javax.swing.UnsupportedLookAndFeelException;
 
 import main.app.MHzCheck;
 import main.app.ModelCheck;
@@ -17,6 +17,20 @@ public class Main {
 	static MHzCheck MHzCheck = new MHzCheck();
 	
 	public static void main(String[] args) {
+		
+		for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+		    if ("com.sun.java.swing.plaf.gtk.GTKLookAndFeel".equals(info.getClassName())) {   
+		       try {
+				javax.swing.UIManager.setLookAndFeel(info.getClassName());
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+					| UnsupportedLookAndFeelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		       break;
+		     } 
+		}
+		
 		Runnable run = new Runnable() {
 		    public void run() {
 				new tempCheck().check();
